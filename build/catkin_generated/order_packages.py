4f74c189e4a99b02f3f60bb1d8fa8a0dbc2c99d8
AntoBrandi <antonio.brandi@outlook.it>
2020-06-09 16:06:28 +0000

    added the kinematics for the robot with the end effector

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/ubuntu/arduinobot/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/ubuntu/arduinobot/devel;/opt/ros/melodic'.split(';') if '/home/ubuntu/arduinobot/devel;/opt/ros/melodic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,9 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/ubuntu/arduinobot/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/ubuntu/arduinobot/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/ubuntu/arduinobot/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/ubuntu/arduinobot/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/ubuntu/arduinobot/devel/share/common-lisp"
-export ROS_PACKAGE_PATH="/home/legion/arduinobot/src:/home/ubuntu/arduinobot/src:$ROS_PACKAGE_PATH"
