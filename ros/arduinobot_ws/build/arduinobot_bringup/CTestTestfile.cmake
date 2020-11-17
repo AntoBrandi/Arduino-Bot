daef88de1d2794c9540b7ade6f33f273959de0d1
abrandi@codearchitects.com <abrandi@codearchitects.com>
2020-11-17 11:18:02 +0100

    removed useless build file

--- ros/arduinobot_ws/build/arduinobot_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/omncbar/arduinobot/arduinobot_ws/src/arduinobot_bringup
-# Build directory: /home/omncbar/arduinobot/arduinobot_ws/build/arduinobot_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros/arduinobot_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/omncbar/arduinobot/arduinobot_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/omncbar/arduinobot/arduinobot_ws/devel;/home/omncbar/ros_tutorial/devel;/home/omncbar/aptius2/devel;/opt/ros/kinetic'.split(';') if '/home/omncbar/arduinobot/arduinobot_ws/devel;/home/omncbar/ros_tutorial/devel;/home/omncbar/aptius2/devel;/opt/ros/kinetic' != '' else []
