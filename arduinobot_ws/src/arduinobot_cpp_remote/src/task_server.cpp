7c705b8c82f368bcaded4d74df7a86470ff01309
AntoBrandi <antonio.brandi@outlook.it>
2023-04-05 20:29:42 +0000

    started arduinobot_remote migration

---
+++ arduinobot_ws/src/arduinobot_cpp_remote/src/task_server.cpp
@@ -0,0 +1,64 @@
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <rclcpp_components/register_node_macro.hpp>
+#include "arduinobot_msgs/action/arduinobot_task.hpp"
+
+#include <memory>
+
+
+using namespace std::placeholders;
+
+namespace arduinobot_cpp_remote
+{
+class TaskServer : public rclcpp::Node
+{
+public:
+  explicit TaskServer(const rclcpp::NodeOptions& options = rclcpp::NodeOptions())
+    : Node("task_server", options)
+  {
+    RCLCPP_INFO(get_logger(), "Starting the Server");
+    action_server_ = rclcpp_action::create_server<arduinobot_msgs::action::ArduinobotTask>(
+        this, "task_server", std::bind(&TaskServer::goalCallback, this, _1, _2),
+        std::bind(&TaskServer::cancelCallback, this, _1),
+        std::bind(&TaskServer::acceptedCallback, this, _1));
+  }
+
+private:
+  rclcpp_action::Server<arduinobot_msgs::action::ArduinobotTask>::SharedPtr action_server_;
+
+  rclcpp_action::GoalResponse goalCallback(
+      const rclcpp_action::GoalUUID& uuid,
+      std::shared_ptr<const arduinobot_msgs::action::ArduinobotTask::Goal> goal)
+  {
+    RCLCPP_INFO(get_logger(), "Received goal request with id %d", goal->task_number);
+    // TODO: check if goal is valid before accepting
+    (void)uuid;
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
+  rclcpp_action::CancelResponse cancelCallback(
+      const std::shared_ptr<rclcpp_action::ServerGoalHandle<arduinobot_msgs::action::ArduinobotTask>> goal_handle)
+  {
+    RCLCPP_INFO(get_logger(), "Received request to cancel goal");
+    // TODO: do something to stop MoveIt!
+    (void)goal_handle;
+    return rclcpp_action::CancelResponse::ACCEPT;
+  }
+
+  void acceptedCallback(
+      const std::shared_ptr<rclcpp_action::ServerGoalHandle<arduinobot_msgs::action::ArduinobotTask>> goal_handle)
+  {
+    // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+    std::thread{ std::bind(&TaskServer::execute, this, _1), goal_handle }.detach();
+  }
+
+  void execute(const std::shared_ptr<rclcpp_action::ServerGoalHandle<arduinobot_msgs::action::ArduinobotTask>> goal_handle)
+  {
+    RCLCPP_INFO(get_logger(), "Executing goal");
+    (void)goal_handle;
+    // TODO: Logic with MoveIt interface
+  }
+};
+}  // namespace arduinobot_cpp_remote
+
+RCLCPP_COMPONENTS_REGISTER_NODE(arduinobot_cpp_remote::TaskServer)
