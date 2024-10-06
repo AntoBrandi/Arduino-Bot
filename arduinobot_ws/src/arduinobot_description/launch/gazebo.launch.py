5ecd1a3f574316ecdd8d84a060bf8b692cd8cccd
AntoBrandi <antonio.brandi@outlook.it>
2024-10-06 13:27:50 +0200

    added RGB camera

--- arduinobot_ws/src/arduinobot_description/launch/gazebo.launch.py
+++ arduinobot_ws/src/arduinobot_description/launch/gazebo.launch.py
@@ -29,6 +29,7 @@ def generate_launch_description():
     
     ros_distro = os.environ['ROS_DISTRO']
     is_ignition = 'True' if ros_distro == 'humble' else 'False'
+    physics_engine = '' if ros_distro == 'humble' else '--physics-engine gz-physics-bullet-featherstone-plugin'
 
     robot_description = ParameterValue(Command([
             'xacro ',
@@ -42,14 +43,15 @@ def generate_launch_description():
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
+        parameters=[{'robot_description': robot_description,
+                     'use_sim_time': True}]
     )
 
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
                 launch_arguments=[
-                    ('gz_args', [' -v 4', ' -r']
+                    ('gz_args', [' -v 4 -r empty.sdf ', physics_engine]
                     )
                 ]
              )
@@ -62,10 +64,21 @@ def generate_launch_description():
                    '-name', 'arduinobot'],
     )
 
+    gz_ros2_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            '/image_raw@sensor_msgs/msg/Image[gz.msgs.Image',
+            '/camera_info@sensor_msgs/msg/CameraInfo[gz.msgs.CameraInfo',
+        ]
+    )
+
     return LaunchDescription([
         model_arg,
         gazebo_resource_path,
         robot_state_publisher_node,
         gazebo,
         gz_spawn_entity,
+        gz_ros2_bridge
     ])
